8003ef0539c08bd7ec5748a97d2ce72170661d9d
RoryMcDermid <97927154+RoryMcDermid@users.noreply.github.com>
2025-09-19 17:48:33 +0100

    Create startup.lua

---
+++ AmethystMining/startup.lua
@@ -0,0 +1,52 @@
+
+--this is run when there is space above
+local function CircleMineUp() 
+  turtle.up()
+  for i = 0,4 do
+    local success, data = turtle.inspect()
+    if success then
+      --likely spot to break, needs tested
+      if data.name = "amethyst_cluster" then
+        turtle.dig()
+      end
+    end
+    turtle.turnLeft()
+  end
+end
+
+local function GoDown()
+  while not turtle.detectDown() do
+    turtle.down()
+  end
+end
+    
+
+
+
+--Code Starts
+
+GoDown()
+while true do
+  local boolean Space = true
+
+  for i = 1,16 do
+    if turtle.getItemCount(i) > 0 then
+      turtle.select(i)
+      if turtle.dropDown() then
+        i = 16
+        Space = false
+      end
+    end
+  end
+  turtle.select(1)
+  sleep(300)
+  if Space = True then
+    while turtle.getItemCount(16) == 0 and not turtle.detectUp() do
+      CircleMineUp()
+    end
+    GoDown()
+  end
+end
+      
+  
+  
